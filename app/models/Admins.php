3f4ccd7248c02297bab40f36fd88eb045968f8a7
gancialucas <lucasgancia@gmail.com>
2022-10-12 00:09:29 -0300

    Metodo Admins modificado. Agregamos getUser

--- app/models/Admins.php
+++ app/models/Admins.php
@@ -5,4 +5,11 @@ class Admins {
     public function __construct() {
         $this->db = new DataBase;
     }
+
+    public function getUser(){
+        $this->db->query('SELECT * FROM users ');
+        $results = $this->db->registers();
+        
+        return $results;
+    }
 }
