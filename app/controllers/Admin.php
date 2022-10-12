e7c6525e39a3d8506cf449bda117b86b7711c8ce
gancialucas <lucasgancia@gmail.com>
2022-10-12 00:05:11 -0300

    agregamos metodo getUser y modificamos controlador

--- app/controllers/Admin.php
+++ app/controllers/Admin.php
@@ -1,15 +1,22 @@
 <?php
-    class Admin extends MainController {
-        public function __construct(){
-            $this->userModel = $this->model('Admins');
-        }
-
-        /*********************************************
-            FUNCIÓN PARA MOSTAR PAGINA ADMIN
-        **********************************************/
-        public function index(){
-            $this->views('pages/admin');
-        }
-
-        
+class Admin extends MainController
+{
+    public function __construct(){
+        $this->userModel = $this->model('Admins');
     }
+
+    /*********************************************
+            FUNCIÓN PARA MOSTAR PAGINA ADMIN
+     **********************************************/
+    public function index(){
+        // Obtener lo usuarios 
+        $users = $this->userModel->getUser();
+
+        $data = [
+            'users' => $users
+        ];
+
+
+        $this->views('pages/admin', $users);
+    }
+}
